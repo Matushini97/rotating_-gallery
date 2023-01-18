dd08e48666e9c85062b4668fc68f5c9c4301d1bd
Matushini97 <matushin.97@gmail.com>
2023-01-18 09:25:26 +0300

    init repo

---
+++ index.js
@@ -0,0 +1,34 @@
+const imageCeontainerEl = document.querySelector('.image-container');
+const prevEl = document.getElementById("prev");
+const nextEl = document.getElementById("next");
+
+let x = 0;
+let timer;
+
+prevEl.addEventListener('click', () => {
+    x += 45;
+    clearTimeout(timer);
+    updateGallery()
+});
+
+nextEl.addEventListener('click', () => {
+    x -= 45;
+    clearTimeout(timer);
+    updateGallery()
+});
+
+function updateGallery() {
+    imageCeontainerEl.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+    timer = setTimeout(() => {
+        x -= 45;
+        updateGallery();
+    }, 3000)
+}
+
+updateGallery();
+
+
+
+
+
+
